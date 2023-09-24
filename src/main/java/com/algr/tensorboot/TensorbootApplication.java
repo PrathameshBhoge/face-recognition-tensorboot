cd0e0c0468a53656699b3dd95ab1751b934b0323
Prathamesh Bhoge <Prathameshbhoge.cool@gmail.com>
2023-09-24 18:42:02 +0530

    Initial commit

---
+++ src/main/java/com/algr/tensorboot/TensorbootApplication.java
@@ -0,0 +1,13 @@
+package com.algr.tensorboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensorbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensorbootApplication.class, args);
+		System.out.println("Hey Boi!");
+	}
+}
